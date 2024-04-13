80f76ebf2fd47fec68d10c1616f5b0e631b18ac8
Радмир Исламов <therad445@outlook.com>
2024-04-13 15:27:08 +0300

    Edit Lab1

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 
 // Абстрактные базовые классы всех видов водителей
 class Driver {
@@ -8,22 +9,85 @@ class Driver {
         virtual ~Driver() {}
 };
 
-class BusDriver {
+// Классы всех видов водителей
+
+class BusDriver: public Driver {
     public:
-        virtual void info() = 0;
-        virtual ~BusDriver() {}
+        virtual void info() {
+            cout << "BusDriver" << endl;
+        }
 };
 
 
-class TaxiDriver {
+class TaxiDriver: public Driver {
     public:
-        virtual void info() = 0;
-        virtual ~TaxiDriver() {}
+        virtual void info() {
+            cout << "TaxiDriver" << endl;
+        }
 };
 
-// 
+// Абстрактная фабрика для производства Водителей
+class BoardAnyCar 
+{
+    public:
+        virtual Driver* createDriver() = 0;
+        virtual ~BoardAnyCar() {}
+};
+
+// Фабрика для производитства Водителей Автобусов и Таксистов 
+class BoardBus: public BoardAnyCar 
+{
+    public:
+        Driver* createDriver() {
+            return new BusDriver;
+        }
+};
+
+ 
+class BoardTaxi: public BoardAnyCar
+{
+    public:
+        Driver* createDriver() {
+            return new TaxiDriver;
+        }
+};
+
+
+class Drivers
+{
+    public:
+        ~Drivers() {
+            for(int i=0; i<vd.size(); ++i) delete vd[i];
+        }
+        void info() {
+            for(int i=0; i<vd.size(); ++i) vd[i]->info();
+        }
+        vector<Driver*> vd;
+};
+
+class Passenger
+{
+    public:
+        Drivers* createDriver (BoardAnyCar& factory) {
+            Drivers* p = new Drivers;
+            p->vd.push_back(factory.createDriver());
+            return p;
+        }
+};
 
 
 int main (){
+    Passenger passenger;
+    BoardBus bb_factory;
+    BoardTaxi bt_factory;
+
+    Drivers * bb = passenger.createDriver(bb_factory);
+    Drivers * bt = passenger.createDriver(bt_factory);
+    cout << "Bus:" << endl;
+    bb->info();
+    cout << "Taxi:" << endl;
+    bt->info();
+    delete bb;
+    delete bt;
     return 0;
 }
